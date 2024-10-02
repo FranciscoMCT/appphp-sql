1ebfce5630de7f05f9496f1561b16e2de176bf53
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-02 11:04:14 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,15 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sqlserver63ujiagifce6s.database.windows.net,1433; Database = sampledb", "sql", "P@$$W)RD1234");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+$serverName = "tcp:sqlserver63ujiagifce6s.database.windows.net,1433"; // Substitua pelo seu servidor
+$database = "sampledb"; // Substitua pelo seu banco de dados
+$username = "sql"; // Substitua pelo seu usuário
+$password = "P@$$W)RD1234"; // Substitua pela sua senha
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "sql", "pwd" => "P@$$W)RD1234", "Database" => "sampledb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sqlserver63ujiagifce6s.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+try {
+    // Cria a conexão com o banco de dados usando PDO
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+    die();
+}
 ?>
