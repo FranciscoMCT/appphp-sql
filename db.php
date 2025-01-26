28b1f91545931ac092d6a40fd98d13549cbb2545
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2025-01-26 00:37:03 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $serverName = "tcp:dbserverdotiofran.database.windows.net,1433"; // Substitua pelo seu servidor
 $database = "banco001"; // Substitua pelo seu banco de dados
-$user = "sql"; // Substitua pelo seu usuário
+$username = "sql"; // Substitua pelo seu usuário
 $password = "Password#123456"; // Substitua pela sua senha
 
 try {
     // Cria a conexão com o banco de dados usando PDO
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $user, $password);
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erro na conexão: " . $e->getMessage();
